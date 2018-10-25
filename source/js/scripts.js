8bad6a8397fe7dce1dbabc700359e6452b72be49
Olya <olya.bachinina@yandex.ru>
2018-10-25 17:20:44 +0300

    Сборка проекта

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,6 +1,9 @@
 var headerMenu = document.querySelector(".header__menu");
-
 var headerButtonMenu = document.querySelector(".header__button-menu");
+var orderButton = document.querySelector(".main-product__button");
+var productItem = document.querySelector(".product__price");
+var modalPurchase = document.querySelector(".purchase__container");
+var modalOverlay = document.querySelector(".modal--purchase");
 
 headerButtonMenu.classList.remove("header__button-menu--no-js");
 
@@ -14,4 +17,14 @@ headerButtonMenu.addEventListener("click", function() {
     headerButtonMenu.classList.remove("header__button-menu--close");
     headerButtonMenu.classList.add("header__button-menu--open");
   }
+});
+
+orderButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalOverlay.classList.remove("visually-hidden");
+});
+
+productItem.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalOverlay.classList.remove("visually-hidden");
 });
