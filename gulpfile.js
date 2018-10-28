3f42085037b14d6e84be4231023f7bb98c844244
Olya <olya.bachinina@yandex.ru>
2018-10-28 22:06:43 +0300

    Правки-2

--- gulpfile.js
+++ gulpfile.js
@@ -92,29 +92,3 @@ gulp.task("build", function (done) {
     done
   );
 });
-
-gulp.task("css", function () {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
-});
-
- gulp.task("server-source", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-   gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
-});
-
- gulp.task("dev", gulp.series("css", "server-source"));
-
